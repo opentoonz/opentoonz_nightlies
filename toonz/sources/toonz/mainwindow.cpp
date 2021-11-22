f84c65fca23b1b12f4b6b3076ca0fed23a601185
shun-iwasawa <shun.iwasawa@ghibli.jp>
2021-11-22 18:49:42 +0900

    cell mark

--- toonz/sources/toonz/mainwindow.cpp
+++ toonz/sources/toonz/mainwindow.cpp
@@ -2822,6 +2822,16 @@ void MainWindow::defineActions() {
   createStopMotionAction(MI_StopMotionToggleUseLiveViewImages,
                          QT_TR_NOOP("Show original live view images."), "");
 #endif  // x64
+
+  // create cell mark actions
+  for (int markId = 0; markId < 12; markId++) {
+    std::string cmdId = (std::string)MI_SetCellMark + std::to_string(markId);
+    std::string labelStr =
+        QT_TR_NOOP("Set Cell Mark ") + std::to_string(markId);
+    QAction *action =
+        createAction(cmdId.c_str(), labelStr.c_str(), "", CellMarkCommandType);
+    action->setData(markId);
+  }
 }
 
 //-----------------------------------------------------------------------------
