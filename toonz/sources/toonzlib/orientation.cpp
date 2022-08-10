95057dd1d199d9c63e003426e4fe7560ceab91ff
shun-iwasawa <shun.iwasawa@ghibli.jp>
2022-08-10 14:42:44 +0900

    expand xsheet frame area

--- toonz/sources/toonzlib/orientation.cpp
+++ toonz/sources/toonzlib/orientation.cpp
@@ -918,6 +918,7 @@ TopToBottomOrientation::TopToBottomOrientation() {
   addDimension(PredefinedDimension::CENTER_ALIGN, Qt::AlignHCenter);
   addDimension(PredefinedDimension::CAMERA_LAYER, CAMERA_CELL_WIDTH);
   addDimension(PredefinedDimension::SCALE_THRESHOLD, 57);
+  addDimension(PredefinedDimension::FRAME_AREA_EXPANSION, PLAY_RANGE_X);
 
   //
   // Paths
@@ -1338,6 +1339,7 @@ LeftToRightOrientation::LeftToRightOrientation() {
   addDimension(PredefinedDimension::CENTER_ALIGN, Qt::AlignVCenter);
   addDimension(PredefinedDimension::CAMERA_LAYER, CAMERA_CELL_HEIGHT);
   addDimension(PredefinedDimension::SCALE_THRESHOLD, 50);
+  addDimension(PredefinedDimension::FRAME_AREA_EXPANSION, 0);  // not used
 
   //
   // Paths
