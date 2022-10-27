eec7c37fc78ce2781941039b1fb937a939327f5d
Rodney <rodney.baker@gmail.com>
2022-10-27 12:36:13 -0500

    Revert "solving linker issue on customized libtiff"

--- toonz/cmake/FindTIFF.cmake
+++ toonz/cmake/FindTIFF.cmake
@@ -19,15 +19,14 @@ endif()
 find_library(
     TIFF_LIBRARY
     NAMES
-        # libtiff.a 
-        # use shared library instead of static
-        libtiff.dylib
+        libtiff.a
     HINTS
         ${SDKROOT}
     PATH_SUFFIXES
         tiff-4.0.3/libtiff/.libs
     NO_DEFAULT_PATH
 )
+
 message("***** libtiff Header path:" ${TIFF_INCLUDE_DIR})
 message("***** libtiff Library path:" ${TIFF_LIBRARY})
 
