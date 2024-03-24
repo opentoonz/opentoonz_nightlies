33a8e0bebce12a4ba57f79a8b7c79c7aadc68a34
konero <19820721+konero@users.noreply.github.com>
2024-03-24 13:31:22 +0000

    function group and channel name colors

--- toonz/sources/toonzqt/functionsheet.cpp
+++ toonz/sources/toonzqt/functionsheet.cpp
@@ -394,7 +394,7 @@ void FunctionSheetColumnHeadViewer::paintEvent(QPaintEvent *e) {
                        getViewer()->getColumnHeaderBorderColor());
 
     // channel name
-    painter.setPen(getViewer()->getTextColor());
+    painter.setPen(getViewer()->getChannelNameTextColor());
     if (channel->isCurrent())
       painter.setPen(m_sheet->getViewer()->getCurrentTextColor());
 
@@ -415,7 +415,7 @@ void FunctionSheetColumnHeadViewer::paintEvent(QPaintEvent *e) {
     // group name
     if (firstGroupColumn) {
       int tmpwidth = (lastGroupColumn) ? width : width * 2;
-      painter.setPen(getViewer()->getTextColor());
+      painter.setPen(getViewer()->getGroupNameTextColor());
       if (group == currentGroup)
         painter.setPen(m_sheet->getViewer()->getCurrentTextColor());
       text = group->getShortName();
